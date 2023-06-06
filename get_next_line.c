772ad5c45cf9f366e5fc11f0a72da7b377318d8f
Jaehee Jung <wogml414@gmail.com>
2023-06-06 23:26:49 +0900

    start

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jaehejun <jaehejun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 20:04:32 by jaehejun          #+#    #+#             */
-/*   Updated: 2023/06/05 23:16:41 by jaehejun         ###   ########.fr       */
+/*   Updated: 2023/06/06 19:16:30 by jaehejun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 #include <fcntl.h>
 #include <string.h>
 
-//size_t	ft_strlen(const char *s)
-//{
-//	size_t	i;
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
 
-//	i = 0;
-//	while (s[i] != '\0')
-//		i++;
-//	return (i);
-//}
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
 
 //char	*ft_strchr(const char *s, int c)
 //{
@@ -107,47 +107,83 @@
 //	return (line);
 //}
 
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*join;
+	size_t	len1;
+	size_t	len2;
+	int		i;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	join = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+	if (join == 0)
+		return (NULL);
+	i = 0;
+	while (len1-- > 0)
+		join[i++] = *s1++;
+	while (len2-- > 0)
+		join[i++] = *s2++;
+	join[i] = '\0';
+	return (join);
+}
+
 char	*get_next_line(int fd)
 {
 	char		*buffer;
 	char		*line;
-	//char		*new_line;
+	char		*new_line;
 	static char	*remain;
 	int			count;
 	int			i;
+	int			j;
 
+	
+	
 	i = -1;
+	j = 0;
 	buffer = malloc(BUFFER_SIZE + 1);
 	if (buffer == NULL)
 		return (NULL);
 	count = read(fd, buffer, BUFFER_SIZE);
+	
 	if (count == -1 || count == 0)
 		return (NULL);
+		
 	buffer[BUFFER_SIZE] = '\0';
+	
 	line = malloc(count + 1);
 	if (line == NULL)
 		return (NULL);
 	line[count] = '\0';
+		
 	while (buffer[++i] != '\0')
 	{
 		line[i] = buffer[i];
-		printf("%c", line[i]);
-		printf("%c", buffer[i]);
 		if (buffer[i] == '\n')
 		{
 			line[i] = buffer[i];
 			break ;
 		}
 	}
-	line[i + 1] = '\0';
-	printf("%p", remain);
-	//while (buffer[i] != '\0')
-	//	*(remain++) = buffer[i++];
-	//*remain = '\0';
-	printf("%s", line);
-	//printf("%s", remain);
+	i++;
+	line[i] = '\0';
 	
+	remain = malloc(BUFFER_SIZE + 1);
+	if (remain == NULL)
+		return (NULL);
+		
+	while (buffer[i] != '\0')
+		remain[j++] = buffer[i++];
+	remain[j] = '\0';
 	
+	new_line = malloc(BUFFER_SIZE + 1);
+	if (new_line == NULL)
+		return (NULL);
+	ft_strjoin(new_line, remain); printf("new_line : %s\n", new_line);
+	printf("line : %s\n", line);
+	printf("remain : %s\n", remain);
+
 	return (line);
 }
 
@@ -160,5 +196,14 @@ int	main(void)
 	fd = open("a.txt", O_RDONLY);
 	//while (line != NULL) 
 	//printf("%s", line = get_next_line(fd));
+	printf("1st\n");
+	get_next_line(fd);
+	printf("2nd\n");
+	get_next_line(fd);
+	printf("3rd\n");
+	get_next_line(fd);
+	printf("4th\n");
+	get_next_line(fd);
+	printf("5th\n");
 	get_next_line(fd);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jaehejun <jaehejun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 20:04:16 by jaehejun          #+#    #+#             */
-/*   Updated: 2023/06/05 21:25:51 by jaehejun         ###   ########.fr       */
+/*   Updated: 2023/06/06 17:06:25 by jaehejun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 5
+#  define BUFFER_SIZE 1
 # endif
 
 # include <stdlib.h>
